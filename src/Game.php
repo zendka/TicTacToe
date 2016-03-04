f5f88230508f8d17fceb82f4e29a59ce43b6f2db
Florin Buga <florin.buga@gmail.com>
2016-03-04 20:22:49 +0000

    Add Game.php and GameTest.php

---
+++ src/Game.php
@@ -0,0 +1,19 @@
+<?php namespace Florin\TicTacToe;
+
+class Game
+{
+    const HUMAN_VS_COMPUTER    = 1;
+    const COMPUTER_VS_HUMAN    = 2;
+    const HUMAN_VS_HUMAN       = 3;
+    const COMPUTER_VS_COMPUTER = 4;
+
+    private $type = self::HUMAN_VS_COMPUTER;
+    private $grid;
+
+    // @todo Validate input
+    public function __construct(Grid $grid, $type = self::HUMAN_VS_COMPUTER)
+    {
+        $this->grid = $grid;
+        $this->type = $type;
+    }
+}
---
+++ tests/GameTest.php
@@ -0,0 +1,9 @@
+<?php namespace Florin\TicTacToe;
+
+class GameTest extends \PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
+}
