af8c5d5ff08b940fd2ad05917a076b500df71aa3
Florin Buga <florin.buga@gmail.com>
2016-03-08 22:15:54 +0000

    Extract methods into a new class: NewellSimonBot

--- src/Game.php
+++ src/Game.php
@@ -8,13 +8,15 @@ class Game
     const COMPUTER_VS_COMPUTER = 4;
 
     private $type = self::HUMAN_VS_COMPUTER;
-    private $grid;
+    public $grid;
+    private $bot;
 
     // @todo Validate input
-    public function __construct(Grid $grid, $type = self::HUMAN_VS_COMPUTER)
+    public function __construct(Grid $grid, $type = self::HUMAN_VS_COMPUTER, $bot = 'NewellSimonBot')
     {
         $this->grid = $grid;
         $this->type = $type;
+        $this->bot = $bot;
     }
 
     public function getState()
@@ -34,7 +36,9 @@ class Game
         }
 
         $currentPlayer = $this->getCurrentPlayer();
-        $bestPosition = $this->getBestMove($currentPlayer);
+
+        $bot = new NewellSimonBot($this);
+        $bestPosition = $bot->getBestMove($currentPlayer);
 
         $this->grid->markPosition($currentPlayer, $bestPosition);
     }
@@ -61,98 +65,9 @@ class Game
         return $this->isFirstPlayersTurn() ? 1 : 2;
     }
 
-    private static function random(array $positions)
-    {
-        return $positions[array_rand($positions)];
-    }
-
-    // Use Newell and Simon's algorithm: https://en.wikipedia.org/wiki/Tic-tac-toe
-    private function getBestMove($player)
-    {
-        $opponent = $player == 1 ? 2 : 1;
-
-        if ($winningPositions = $this->getWinningPositions($player)) {
-            $bestMoves = $winningPositions;
-        } elseif ($opponentsWinningPositions = $this->getWinningPositions($opponent)) {
-            // Block opponent by marking its winning position
-            $bestMoves = $opponentsWinningPositions;
-        } elseif ($forkPositions = $this->getForkPositions($player)) {
-            $bestMoves = $forkPositions;
-        } elseif ($opponentsForkPositions = $this->getForkPositions($opponent)) {
-            $canBlockOpponent = sizeof($opponentsForkPositions) == 1;
-            if ($canBlockOpponent) {
-                $bestMoves = $opponentsForkPositions;
-            } else {
-                $forceOpponentPositions = $this->getForceOpponentPositions($player, $opponentsForkPositions);
-                $bestMoves = $forceOpponentPositions;
-            }
-        } elseif ($availableCentralPosition = array_intersect($this->grid->getAvailablePositions(), Grid::CENTER)) {
-            $bestMoves = $availableCentralPosition;
-        } elseif ($availableCornerPositions = array_intersect($this->grid->getAvailablePositions(), Grid::CORNERS)) {
-            $bestMoves = $availableCornerPositions;
-        } else {
-            $bestMoves = $this->grid->getAvailablePositions();
-        }
-
-        return self::random($bestMoves);
-    }
-
-    private function getWinningPositions($player)
-    {
-        return array_filter($this->grid->getAvailablePositions(),
-          function ($position) use ($player) {
-              $this->grid->markPosition($player, $position);
-              $isWinningPosition = $this->isWinner($player);
-              $this->grid->cancelLastMark($player);
-              return $isWinningPosition;
-          }
-        );
-    }
-
-    private function isWinner($player)
+    public function isWinner($player)
     {
         return $this->grid->hasThreeInLine($player);
-    }
-
-    /**
-     * Gets fork positions for player
-     *
-     * A fork position is an unmarked position that if marked creates two future winning positions.
-     * The opponent can only block one. So this is a future win.
-     */
-    private function getForkPositions($player)
-    {
-        return array_filter($this->grid->getAvailablePositions(),
-          function ($position) use ($player) {
-              $this->grid->markPosition($player, $position);
-              $isForkPosition = count($this->getWinningPositions($player)) >= 2;
-              $this->grid->cancelLastMark($player);
-
-              return $isForkPosition;
-          }
-        );
-    }
-
-    /**
-     * Get the "force opponent" positions
-     *
-     * A "force opponent" position is one that creates a future winning position
-     * The opponent will be forced to block this position in order not to lose.
-     * Make sure that the position to be blocked by the opponent is not a winning one for him.
-     */
-    private function getForceOpponentPositions($player, $opponentsForkPositions)
-    {
-        return array_filter($this->grid->getAvailablePositions(),
-          function ($position) use ($player, $opponentsForkPositions) {
-              $this->grid->markPosition($player, $position);
-              $winningPositions = $this->getWinningPositions($player);
-              $isForceOpponentPosition = !empty($winningPositions) &&
-                                         !array_intersect($winningPositions, $opponentsForkPositions);
-              $this->grid->cancelLastMark($player);
-
-              return $isForceOpponentPosition;
-          }
-        );
     }
 
     public function isOver()
---
+++ src/NewellSimonBot.php
@@ -0,0 +1,105 @@
+<?php namespace Florin\TicTacToe;
+
+/**
+ * Class NewellSimonBot
+ *
+ * Uses Newell and Simon's algorithm to play a TicTacToe game:
+ * https://en.wikipedia.org/wiki/Tic-tac-toe
+ */
+class NewellSimonBot
+{
+    private $game;
+
+    public function __construct(Game $game)
+    {
+        $this->game = $game;
+    }
+
+    public function getBestMove($player)
+    {
+        $opponent = $player == 1 ? 2 : 1;
+
+        if ($winningPositions = $this->getWinningPositions($player)) {
+            $bestMoves = $winningPositions;
+        } elseif ($opponentsWinningPositions = $this->getWinningPositions($opponent)) {
+            // Block opponent by marking its winning position
+            $bestMoves = $opponentsWinningPositions;
+        } elseif ($forkPositions = $this->getForkPositions($player)) {
+            $bestMoves = $forkPositions;
+        } elseif ($opponentsForkPositions = $this->getForkPositions($opponent)) {
+            $canBlockOpponent = sizeof($opponentsForkPositions) == 1;
+            if ($canBlockOpponent) {
+                $bestMoves = $opponentsForkPositions;
+            } else {
+                $forceOpponentPositions = $this->getForceOpponentPositions($player, $opponentsForkPositions);
+                $bestMoves = $forceOpponentPositions;
+            }
+        } elseif ($availableCentralPosition = array_intersect($this->game->grid->getAvailablePositions(), Grid::CENTER)) {
+            $bestMoves = $availableCentralPosition;
+        } elseif ($availableCornerPositions = array_intersect($this->game->grid->getAvailablePositions(), Grid::CORNERS)) {
+            $bestMoves = $availableCornerPositions;
+        } else {
+            $bestMoves = $this->game->grid->getAvailablePositions();
+        }
+
+        return self::random($bestMoves);
+    }
+
+    private function getWinningPositions($player)
+    {
+        return array_filter($this->game->grid->getAvailablePositions(),
+          function ($position) use ($player) {
+              $this->game->grid->markPosition($player, $position);
+              $isWinningPosition = $this->game->isWinner($player);
+              $this->game->grid->cancelLastMark($player);
+              return $isWinningPosition;
+          }
+        );
+    }
+
+    /**
+     * Gets fork positions for player
+     *
+     * A fork position is an unmarked position that if marked creates two future winning positions.
+     * The opponent can only block one. So this is a future win.
+     */
+    private function getForkPositions($player)
+    {
+        return array_filter($this->game->grid->getAvailablePositions(),
+          function ($position) use ($player) {
+              $this->game->grid->markPosition($player, $position);
+              $isForkPosition = count($this->getWinningPositions($player)) >= 2;
+              $this->game->grid->cancelLastMark($player);
+
+              return $isForkPosition;
+          }
+        );
+    }
+
+    /**
+     * Get the "force opponent" positions
+     *
+     * A "force opponent" position is one that creates a future winning position
+     * The opponent will be forced to block this position in order not to lose.
+     * Make sure that the position to be blocked by the opponent is not a winning one for him.
+     */
+    private function getForceOpponentPositions($player, $opponentsForkPositions)
+    {
+        return array_filter($this->game->grid->getAvailablePositions(),
+          function ($position) use ($player, $opponentsForkPositions) {
+              $this->game->grid->markPosition($player, $position);
+              $winningPositions = $this->getWinningPositions($player);
+              $isForceOpponentPosition = !empty($winningPositions) &&
+                                         !array_intersect($winningPositions, $opponentsForkPositions);
+              $this->game->grid->cancelLastMark($player);
+
+              return $isForceOpponentPosition;
+          }
+        );
+    }
+
+    private static function random(array $positions)
+    {
+        return $positions[array_rand($positions)];
+    }
+}
